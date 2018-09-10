3b9481c62a52a4a0b71aa15be7c8ea15dc2d753e
brunapcs <Bubupipi70>
2018-09-10 13:04:41 -0300

    TADs de listas e arvores binarias

---
+++ main.c/main.c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  main.c
+//
+//  Created by Bruna Paracat  on 9/10/18.
+//  Copyright © 2018 Bruna Paracat. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
